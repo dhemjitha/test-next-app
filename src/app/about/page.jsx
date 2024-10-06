3445290da5c2227077479ab9c921efba94b74d66
dhemjitha <dhemjitha@gmail.com>
2024-10-06 18:40:12 +0530

    fix: navigate between pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function About() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">About Us Page</h1>
+      <br />
+      <h1><Link href="/">Back to Home</Link></h1>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">Home Page</h1>
+      <br />
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/products">Products</Link>
+        </li>
+      </ul>
     </div>
   );
 }
