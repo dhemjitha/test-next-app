813e01a1513744d1ad4587364122c3d23c7b9f72
dhemjitha <dhemjitha@gmail.com>
2024-11-04 21:26:26 +0530

    fixed: identified remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.prismic.io'],
-    },
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.prismic.io',
+          },
+        ],
+      },
 };
 
 export default nextConfig;
